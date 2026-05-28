7fd1df61a48fab9e424c454d4cfb5ac8f7aef182
sebastian alonso gonzales-portillo <sebasagpf@gmail.com>
2026-05-28 17:56:46 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for word in strs:
+            encoded += f'{len(word)}%{word}'
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0 
+        while i < len(s):
+            j=i
+            while s[j] != '%':
+                j+=1
+            length = int(s[i:j])
+            print(length)
+            word = s[j+1:j+1+length]
+            print(word)
+            res.append(word)
+            i = j+1+length
+        return res
